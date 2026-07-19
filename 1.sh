fa1cce07e5feeeff755a3c2b296cf8ae3f998c3f
lihaoyu <109925622+mingmingzhichu@users.noreply.github.com>
2026-07-19 14:56:48 +0800

    更新 1.sh

--- 1.sh
+++ 1.sh
@@ -6,13 +6,15 @@ cat >> arch/arm64/boot/dts/qcom/sm8150-oneplus-hotdogb.dts << 'EOF'
     touchscreen@20 {
         compatible = "syna,rmi4-s3706b-i2c", "synaptics-s3706";
         reg = <0x20>;
+
         interrupt-parent = <&tlmm>;
         interrupts = <122 IRQ_TYPE_LEVEL_LOW>;
-        reset-gpios = <&tlmm 54 GPIO_ACTIVE_LOW>;   /* 改为 54 */
-        vcc_1v8-supply = <&vreg_l7a_1p8>;           /* 改为实际存在的标签 */
+
+        reset-gpios = <&tlmm 54 GPIO_ACTIVE_LOW>;
+        vcc_1v8-supply = <&vreg_l7a_1p8>;
+
         touchscreen-size-x = <1080>;
         touchscreen-size-y = <2400>;
-        touchscreen-max-pressure = <255>;
     };
 };
 EOF
