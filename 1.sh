0c3d7fbe2f64f5d2a7d463ba1b0a4abd253d7df7
lihaoyu <109925622+mingmingzhichu@users.noreply.github.com>
2026-07-19 13:07:50 +0800

    创建 1.sh

---
+++ 1.sh
@@ -0,0 +1,15 @@
+sed -i '$a\
+&i2c5 {\
+    status = "okay";\
+    touchscreen@20 {\
+        compatible = "syna,rmi4-s3706b-i2c", "synaptics-s3706";\
+        reg = <0x20>;\
+        interrupt-parent = <&tlmm>;\
+        interrupts = <122 IRQ_TYPE_LEVEL_LOW>;\
+        reset-gpios = <&tlmm 80 GPIO_ACTIVE_LOW>;\
+        vcc_1v8-supply = <&pm8150_l6>;\
+        touchscreen-size-x = <1080>;\
+        touchscreen-size-y = <2400>;\
+        touchscreen-max-pressure = <255>;\
+    };\
+};' arch/arm64/boot/dts/qcom/sm8150-oneplus-hotdogb.dts
