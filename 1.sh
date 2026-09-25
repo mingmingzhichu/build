39a541ca6b2b4c28409f99a6670abfce0f8b6b12
lihaoyu <109925622+mingmingzhichu@users.noreply.github.com>
2026-09-25 01:58:11 +0000

    1

--- 1.sh
+++ 1.sh
@@ -23,4 +23,70 @@ cat >> arch/arm64/boot/dts/qcom/sm8150-oneplus-hotdogb.dts << 'EOF'
         touchscreen-size-y = <2400>;
     };
 };
+--- linux/arch/arm64/boot/dts/qcom/sm8150-oneplus-hotdogb.dts	2026-09-24 13:15:33.061836609 +0000
++++ qcom/sm8150-oneplus-hotdogb.dts	2026-09-25 00:52:48.102742944 +0000
+@@ -7,11 +7,12 @@
+ 
+ #include <dt-bindings/regulator/qcom,rpmh-regulator.h>
+ #include <dt-bindings/gpio/gpio.h>
++#include <dt-bindings/usb/pd.h>
+ #include "sm8150-oneplus-common.dtsi"
+ 
+ / {
+ 	model = "OnePlus 7T";
+-	compatible = "oneplus,hotdobg", "oneplus,oneplus7", "qcom,sm8150";
++	compatible = "oneplus,hotdogb", "oneplus,oneplus7", "qcom,sm8150";
+ 
+ 	aliases {
+ 		display0 = &framebuffer0;
+@@ -62,4 +63,49 @@
+ 			ecc-size = <0x0>;
+ 		};
+ 	};
++};
++
++&ufs_mem_hc {
++	/delete-property/ reset-gpios;
++};
++
++&pm8150b_vbus {
++	status = "okay";
++};
++
++&pm8150b_typec {
++	vdd-pdphy-supply = <&vreg_l2a_3p1>;
++	status = "okay";
++
++	connector {
++		compatible = "usb-c-connector";
++		power-role = "dual";
++		data-role = "dual";
++		self-powered;
++
++		source-pdos = <PDO_FIXED(5000, 3000,
++					 PDO_FIXED_DUAL_ROLE |
++					 PDO_FIXED_USB_COMM |
++					 PDO_FIXED_DATA_SWAP)>;
++
++		sink-pdos = <PDO_FIXED(5000, 3000,
++					PDO_FIXED_DUAL_ROLE |
++					PDO_FIXED_USB_COMM |
++					PDO_FIXED_DATA_SWAP)>;
++
++		op-sink-microwatt = <10000000>;
++
++		ports {
++			#address-cells = <1>;
++			#size-cells = <0>;
++
++			port@0 {
++				reg = <0>;
++
++				pm8150b_role_switch_in: endpoint {
++					remote-endpoint = <&usb_1_dwc3_hs>;
++				};
++			};
++		};
+	};
+};
 EOF
